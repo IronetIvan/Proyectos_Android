774af1dad809b3da0036501e3352c76a0749aa6e
Adri <adrian.olmo@cesjuanpablosegundo.es>
2019-10-30 14:23:03 +0100

    Practica Botones

--- PracticaBotones/app/src/main/java/com/example/practicabotones/MainActivity.java
+++ PracticaBotones/app/src/main/java/com/example/practicabotones/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.practicabotones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity  extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton aleatorio, ganador, dados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instancias();
+        acciones();
+    }
+
+    private void acciones() {
+        aleatorio.setOnClickListener(this);
+        ganador.setOnClickListener(this);
+        dados.setOnClickListener(this);
+    }
+
+    private void instancias() {
+        aleatorio = findViewById(R.id.btnaleatorio);
+        ganador = findViewById(R.id.btntrofeo);
+        dados = findViewById(R.id.btndados);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.btnaleatorio:
+                Intent aleatorio = new Intent(getApplicationContext(),AleatorioActivity.class);
+                startActivity(aleatorio);
+                break;
+        }
     }
 }
