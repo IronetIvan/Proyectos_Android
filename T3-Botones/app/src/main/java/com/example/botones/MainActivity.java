7e68d6762866c67713a06e3616846d4a08f71748
Adri <adrian.olmo@cesjuanpablosegundo.es>
2019-10-30 12:22:36 +0100

    Botones Android

--- T3-Botones/app/src/main/java/com/example/botones/MainActivity.java
+++ T3-Botones/app/src/main/java/com/example/botones/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.botones;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton boton;
+    ToggleButton togglebtn;
+    CheckBox checknormal;
+    Switch switchnormal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instancias();
+        acciones();
+    }
+
+    private void acciones() {
+        boton.setOnClickListener(this);
+    }
+
+    private void instancias() {
+        boton = findViewById(R.id.btnimagen);
+        togglebtn = findViewById(R.id.toggleboton);
+        checknormal = findViewById(R.id.checknormal);
+        switchnormal = findViewById(R.id.switchnormal);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnimagen:
+
+                break;
+        }
     }
 }
