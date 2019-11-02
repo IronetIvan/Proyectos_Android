b255246bab816b6764dd8300fe3f816c5c7cf8e9
Adrian <adrian.olmo@cesjuanpabloseguno.es>
2019-11-02 14:43:05 +0100

    Practica Botones

--- PracticaBotones/app/src/main/java/com/example/practicabotones/AleatorioActivity.java
+++ PracticaBotones/app/src/main/java/com/example/practicabotones/AleatorioActivity.java
@@ -3,12 +3,72 @@ package com.example.practicabotones;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.Toast;
 
-public class AleatorioActivity extends AppCompatActivity {
+public class AleatorioActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, RadioGroup.OnCheckedChangeListener {
+
+    ImageButton btncohete;
+    CheckBox permitirdecimales;
+    RadioGroup gruporadios;
+    Switch repetir;
+    EditText decimalesmin, decimalesmax, numdecimales;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aleatorio);
+        instancias();
+        acciones();
+    }
+
+    private void acciones() {
+
+        btncohete.setOnClickListener(this); //accion del boton
+        permitirdecimales.setOnCheckedChangeListener(this); //acciones del Checkbox
+        repetir.setOnCheckedChangeListener(this); //acciones del Switch
+        gruporadios.setOnCheckedChangeListener(this); //acciones del grupo de radios
+    }
+
+    private void instancias() {
+
+        btncohete = findViewById(R.id.btncohete);
+        permitirdecimales = findViewById(R.id.checkpermitirdecimales);
+        gruporadios = findViewById(R.id.grupodecimales);
+        repetir = findViewById(R.id.switchrepetir);
+        decimalesmin = findViewById(R.id.decimalminimo);
+        decimalesmax = findViewById(R.id.decimalmaximo);
+        numdecimales = findViewById(R.id.numerosdecimales);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btncohete:
+                Toast.makeText(getApplicationContext(), "Boton de generar numeros pulsado", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundbutton, boolean b) {
+
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int i) {
+
+        
+
     }
 }
