bb7c2260833ec1d8449c17c390e1d5ab8da9a204
Adri <adrian.olmo@cesjuanpablosegundo.es>
2019-11-06 12:23:52 +0100

    Pasar Datos entre Activitys

--- PasarResultados/app/src/main/java/com/example/pasarresultados/MainActivity.java
+++ PasarResultados/app/src/main/java/com/example/pasarresultados/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.pasarresultados;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnpulsar;
+    TextView txttexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instancias();
+    }
+
+    private void instancias() {
+        btnpulsar = findViewById(R.id.btnpulsar);
+        txttexto = findViewById(R.id.texto);
     }
 }
--- PasarResultados/app/src/main/java/com/example/pasarresultados/SecondActivity.java
+++ PasarResultados/app/src/main/java/com/example/pasarresultados/SecondActivity.java
@@ -3,12 +3,22 @@ package com.example.pasarresultados;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
 
+    Button resultado;
+    EditText editable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        instancias();
+    }
+
+    private void instancias() {
+        resultado = findViewById(R.id.btnpulsar);
+        editable = findViewById(R.id.textoeditado);
     }
 }
