da24c5cece0af30e51d8c4608b40ea2dd4b96d30
Adrian Olmo <adrian.olmo@cesjuanpablosegundo.es>
2019-11-07 20:25:59 +0100

    Practica Botones

--- PracticaBotones/app/src/main/java/com/example/practicabotones/AleatorioActivity.java
+++ PracticaBotones/app/src/main/java/com/example/practicabotones/AleatorioActivity.java
@@ -14,55 +14,66 @@ import android.widget.Toast;
 
 public class AleatorioActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, RadioGroup.OnCheckedChangeListener {
 
-        ImageButton btncohete;
-        CheckBox permitirdecimales;
-        RadioGroup gruporadios;
-        Switch repetir;
-        EditText decimalesmin, decimalesmax, numdecimales;
-
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_aleatorio);
-            instancias();
-            acciones();
-        }
-
-        private void acciones() {
-
-            btncohete.setOnClickListener(this); //accion del boton
-            permitirdecimales.setOnCheckedChangeListener(this); //acciones del Checkbox
-            repetir.setOnCheckedChangeListener(this); //acciones del Switch
-            gruporadios.setOnCheckedChangeListener(this); //acciones del grupo de radios
-        }
-
-        private void instancias() {
-
-            btncohete = findViewById(R.id.btncohete);
-            permitirdecimales = findViewById(R.id.checkpermitirdecimales);
-            gruporadios = findViewById(R.id.grupodecimales);
-            repetir = findViewById(R.id.switchrepetir);
-            decimalesmin = findViewById(R.id.decimalminimo);
-            decimalesmax = findViewById(R.id.decimalmaximo);
-            numdecimales = findViewById(R.id.numerosdecimales);
-
-        }
-
+    ImageButton btncohete;
+    CheckBox permitirdecimales;
+    RadioGroup gruporadios;
+    Switch repetir;
+    EditText decimalesmin, decimalesmax, numdecimales;
 
 
     @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aleatorio);
+        instancias();
+        acciones();
+    }
+
+    private void acciones() {
+
+        btncohete.setOnClickListener(this); //accion del boton
+        permitirdecimales.setOnCheckedChangeListener(this); //acciones del Checkbox
+        repetir.setOnCheckedChangeListener(this); //acciones del Switch
+        gruporadios.setOnCheckedChangeListener(this); //acciones del grupo de radios
+    }
+
+    private void instancias() {
+
+        btncohete = findViewById(R.id.btncohete);
+        permitirdecimales = findViewById(R.id.checkpermitirdecimales);
+        gruporadios = findViewById(R.id.grupodecimales);
+        repetir = findViewById(R.id.switchrepetir);
+        decimalesmin = findViewById(R.id.decimalminimo);
+        decimalesmax = findViewById(R.id.decimalmaximo);
+        numdecimales = findViewById(R.id.numerosdecimales);
+
+    }
+
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int i) {
 
     }
 
     @Override
     public void onClick(View v) {
-        
+
     }
 
     @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+    public void onCheckedChanged(CompoundButton compoundbutton, boolean b) {
 
+
+        switch (compoundbutton.getId()) {
+
+            case R.id.grupodecimales:
+                if (b) {
+                    decimalesmin.setEnabled(false);
+                } else {
+                    decimalesmin.setEnabled(true);
+                }
+
+                break;
+        }
     }
 }
