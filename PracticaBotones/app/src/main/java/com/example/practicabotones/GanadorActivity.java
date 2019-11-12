dae1a0c2460cc4755dfed55dd530de56995d5363
Adri <adrian.olmo@cesjuanpablosegundo.es>
2019-11-12 08:48:10 +0100

    Generador de numeros aleatorios

--- PracticaBotones/app/src/main/java/com/example/practicabotones/GanadorActivity.java
+++ PracticaBotones/app/src/main/java/com/example/practicabotones/GanadorActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +22,7 @@ public class GanadorActivity extends AppCompatActivity implements View.OnClickLi
     TextView parrillaCon;
     EditText concursante;
     ArrayList concursantes;
+    Boolean estado_parrilla;
     int posicion = 0;
 
     @Override
@@ -49,19 +51,29 @@ public class GanadorActivity extends AppCompatActivity implements View.OnClickLi
 
         switch (v.getId()) {
             case R.id.agregarconcursante:
-                String con = concursante.getText().toString();
-                concursantes.add(posicion,con);
-                parrillaCon.append(con + "\n");
-                concursante.setText("");
-                System.out.println(concursantes);
-                posicion++;
+                if (!concursante.getText().toString().isEmpty()) {
+                    String con = concursante.getText().toString();
+                    concursantes.add(posicion, con);
+                    parrillaCon.append(con + "\n");
+                    concursante.setText("");
+                    System.out.println(concursantes);
+                    posicion++;
+                    estado_parrilla = true;
+                } else {
+                    Toast.makeText(getApplicationContext(),"Introduce concursante",Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.generarganador:
-                int aleatorio = (int)(Math.random()*concursantes.size());
-                String prueba = concursantes.get(aleatorio).toString();
-                parrillaCon.setText("El ganador es -" + prueba + "-");
-                concursantes.clear();
-                posicion = 0;
+                if (!parrillaCon.getText().toString().isEmpty()) {
+                    int aleatorio = (int) (Math.random() * concursantes.size());
+                    String prueba = concursantes.get(aleatorio).toString();
+                    parrillaCon.setText("El ganador es -" + prueba + "-");
+                    concursantes.clear();
+                    posicion = 0;
+                    estado_parrilla = false;
+                }else{
+                    Toast.makeText(getApplicationContext(),"No hay concursantes",Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
 
