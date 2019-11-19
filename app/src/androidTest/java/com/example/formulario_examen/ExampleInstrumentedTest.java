87d5d6a8c89d84db956ec13edb7af44d255efa39
Adrian Olmo <adrian.olmo@cesjuanpablosegundo.es>
2019-11-19 18:44:38 +0100

    Formulario Examen

--- app/src/androidTest/java/com/example/formulario_examen/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/formulario_examen/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.formulario_examen;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- app/src/main/java/com/example/formulario_examen/MainActivity.java
+++ app/src/main/java/com/example/formulario_examen/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.formulario_examen;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    final static String TAG_CARGA = "CARGA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler =  new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(), FormularioActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
     }
 }
